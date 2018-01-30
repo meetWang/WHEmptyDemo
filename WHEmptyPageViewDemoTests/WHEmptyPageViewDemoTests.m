01dc898cbe5d56ee7b56ad64a98a9e76e29fdd13
王红 <wanghong@wanghongdeimac.local>
2018-01-30 10:28:04 +0800

    Initial Commit

---
+++ WHEmptyPageViewDemoTests/WHEmptyPageViewDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  WHEmptyPageViewDemoTests.m
+//  WHEmptyPageViewDemoTests
+//
+//  Created by 王红 on 2018/1/30.
+//  Copyright © 2018年 王红. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WHEmptyPageViewDemoTests : XCTestCase
+
+@end
+
+@implementation WHEmptyPageViewDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
